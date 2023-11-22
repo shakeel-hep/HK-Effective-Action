250dedf8e55b9ad44142e7f4775e45552f7cd7ef
Upalaparna Banerjee <62480327+Upalaparna@users.noreply.github.com>
2023-11-22 14:37:34 +0530

    Add files via upload
    Effective operators, given in open-derivative forms.

---
+++ Effective operators (in open derivative form)/For 4 particles/SA2M4.m
@@ -0,0 +1,243 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+-(\[CapitalLambda]1^2*func[tr[D1[\[Mu]14]^2]]) - 
+ \[CapitalLambda]2^2*func[tr[D1[\[Mu]15]^2]] - 
+ \[CapitalLambda]3^2*func[tr[D1[\[Mu]16]^2]] - 
+ \[CapitalLambda]4^2*func[tr[D1[\[Mu]17]^2]] + 
+ (3*M1^4*func[tr[U[1, 2]**U[2, 1]]])/(2*(M1^2 - M2^2)^2) - 
+ (3*M1^2*M2^2*func[tr[U[1, 2]**U[2, 1]]])/(M1^2 - M2^2)^2 + 
+ (3*M2^4*func[tr[U[1, 2]**U[2, 1]]])/(2*(M1^2 - M2^2)^2) - 
+ (3*M1^2*\[CapitalLambda]1^2*func[tr[U[1, 2]**U[2, 1]]])/
+  (2*(M1^2 - M2^2)^2) + (3*M2^2*\[CapitalLambda]1^2*
+   func[tr[U[1, 2]**U[2, 1]]])/(2*(M1^2 - M2^2)^2) - 
+ (\[CapitalLambda]1^2*func[tr[U[1, 2]**U[2, 1]]])/(-M1^2 + M2^2) + 
+ (3*M1^2*\[CapitalLambda]2^2*func[tr[U[1, 2]**U[2, 1]]])/
+  (2*(M1^2 - M2^2)^2) - (3*M2^2*\[CapitalLambda]2^2*
+   func[tr[U[1, 2]**U[2, 1]]])/(2*(M1^2 - M2^2)^2) - 
+ (\[CapitalLambda]2^2*func[tr[U[1, 2]**U[2, 1]]])/(M1^2 - M2^2) + 
+ (3*M1^4*func[tr[U[1, 3]**U[3, 1]]])/(2*(M1^2 - M3^2)^2) - 
+ (3*M1^2*M3^2*func[tr[U[1, 3]**U[3, 1]]])/(M1^2 - M3^2)^2 + 
+ (3*M3^4*func[tr[U[1, 3]**U[3, 1]]])/(2*(M1^2 - M3^2)^2) - 
+ (3*M1^2*\[CapitalLambda]1^2*func[tr[U[1, 3]**U[3, 1]]])/
+  (2*(M1^2 - M3^2)^2) + (3*M3^2*\[CapitalLambda]1^2*
+   func[tr[U[1, 3]**U[3, 1]]])/(2*(M1^2 - M3^2)^2) - 
+ (\[CapitalLambda]1^2*func[tr[U[1, 3]**U[3, 1]]])/(-M1^2 + M3^2) + 
+ (3*M1^2*\[CapitalLambda]3^2*func[tr[U[1, 3]**U[3, 1]]])/
+  (2*(M1^2 - M3^2)^2) - (3*M3^2*\[CapitalLambda]3^2*
+   func[tr[U[1, 3]**U[3, 1]]])/(2*(M1^2 - M3^2)^2) - 
+ (\[CapitalLambda]3^2*func[tr[U[1, 3]**U[3, 1]]])/(M1^2 - M3^2) + 
+ (3*M1^4*func[tr[U[1, 4]**U[4, 1]]])/(2*(M1^2 - M4^2)^2) - 
+ (3*M1^2*M4^2*func[tr[U[1, 4]**U[4, 1]]])/(M1^2 - M4^2)^2 + 
+ (3*M4^4*func[tr[U[1, 4]**U[4, 1]]])/(2*(M1^2 - M4^2)^2) - 
+ (3*M1^2*\[CapitalLambda]1^2*func[tr[U[1, 4]**U[4, 1]]])/
+  (2*(M1^2 - M4^2)^2) + (3*M4^2*\[CapitalLambda]1^2*
+   func[tr[U[1, 4]**U[4, 1]]])/(2*(M1^2 - M4^2)^2) - 
+ (\[CapitalLambda]1^2*func[tr[U[1, 4]**U[4, 1]]])/(-M1^2 + M4^2) + 
+ (3*M1^2*\[CapitalLambda]4^2*func[tr[U[1, 4]**U[4, 1]]])/
+  (2*(M1^2 - M4^2)^2) - (3*M4^2*\[CapitalLambda]4^2*
+   func[tr[U[1, 4]**U[4, 1]]])/(2*(M1^2 - M4^2)^2) - 
+ (\[CapitalLambda]4^2*func[tr[U[1, 4]**U[4, 1]]])/(M1^2 - M4^2) + 
+ (3*M2^4*func[tr[U[2, 3]**U[3, 2]]])/(2*(M2^2 - M3^2)^2) - 
+ (3*M2^2*M3^2*func[tr[U[2, 3]**U[3, 2]]])/(M2^2 - M3^2)^2 + 
+ (3*M3^4*func[tr[U[2, 3]**U[3, 2]]])/(2*(M2^2 - M3^2)^2) - 
+ (3*M2^2*\[CapitalLambda]2^2*func[tr[U[2, 3]**U[3, 2]]])/
+  (2*(M2^2 - M3^2)^2) + (3*M3^2*\[CapitalLambda]2^2*
+   func[tr[U[2, 3]**U[3, 2]]])/(2*(M2^2 - M3^2)^2) - 
+ (\[CapitalLambda]2^2*func[tr[U[2, 3]**U[3, 2]]])/(-M2^2 + M3^2) + 
+ (3*M2^2*\[CapitalLambda]3^2*func[tr[U[2, 3]**U[3, 2]]])/
+  (2*(M2^2 - M3^2)^2) - (3*M3^2*\[CapitalLambda]3^2*
+   func[tr[U[2, 3]**U[3, 2]]])/(2*(M2^2 - M3^2)^2) - 
+ (\[CapitalLambda]3^2*func[tr[U[2, 3]**U[3, 2]]])/(M2^2 - M3^2) + 
+ (3*M2^4*func[tr[U[2, 4]**U[4, 2]]])/(2*(M2^2 - M4^2)^2) - 
+ (3*M2^2*M4^2*func[tr[U[2, 4]**U[4, 2]]])/(M2^2 - M4^2)^2 + 
+ (3*M4^4*func[tr[U[2, 4]**U[4, 2]]])/(2*(M2^2 - M4^2)^2) - 
+ (3*M2^2*\[CapitalLambda]2^2*func[tr[U[2, 4]**U[4, 2]]])/
+  (2*(M2^2 - M4^2)^2) + (3*M4^2*\[CapitalLambda]2^2*
+   func[tr[U[2, 4]**U[4, 2]]])/(2*(M2^2 - M4^2)^2) - 
+ (\[CapitalLambda]2^2*func[tr[U[2, 4]**U[4, 2]]])/(-M2^2 + M4^2) + 
+ (3*M2^2*\[CapitalLambda]4^2*func[tr[U[2, 4]**U[4, 2]]])/
+  (2*(M2^2 - M4^2)^2) - (3*M4^2*\[CapitalLambda]4^2*
+   func[tr[U[2, 4]**U[4, 2]]])/(2*(M2^2 - M4^2)^2) - 
+ (\[CapitalLambda]4^2*func[tr[U[2, 4]**U[4, 2]]])/(M2^2 - M4^2) + 
+ (3*M3^4*func[tr[U[3, 4]**U[4, 3]]])/(2*(M3^2 - M4^2)^2) - 
+ (3*M3^2*M4^2*func[tr[U[3, 4]**U[4, 3]]])/(M3^2 - M4^2)^2 + 
+ (3*M4^4*func[tr[U[3, 4]**U[4, 3]]])/(2*(M3^2 - M4^2)^2) - 
+ (3*M3^2*\[CapitalLambda]3^2*func[tr[U[3, 4]**U[4, 3]]])/
+  (2*(M3^2 - M4^2)^2) + (3*M4^2*\[CapitalLambda]3^2*
+   func[tr[U[3, 4]**U[4, 3]]])/(2*(M3^2 - M4^2)^2) - 
+ (\[CapitalLambda]3^2*func[tr[U[3, 4]**U[4, 3]]])/(-M3^2 + M4^2) + 
+ (3*M3^2*\[CapitalLambda]4^2*func[tr[U[3, 4]**U[4, 3]]])/
+  (2*(M3^2 - M4^2)^2) - (3*M4^2*\[CapitalLambda]4^2*
+   func[tr[U[3, 4]**U[4, 3]]])/(2*(M3^2 - M4^2)^2) - 
+ (\[CapitalLambda]4^2*func[tr[U[3, 4]**U[4, 3]]])/(M3^2 - M4^2) - 
+ (func[tr[D1^4]]*Log[\[CapitalLambda]1^2])/2 + 
+ \[CapitalLambda]1^2*func[tr[D1[\[Mu]14]^2]]*Log[\[CapitalLambda]1^2] - 
+ func[tr[D1^2**U[1, 1]]]*Log[\[CapitalLambda]1^2] + 
+ (\[CapitalLambda]1^2*func[tr[U[1, 2]**U[2, 1]]]*Log[\[CapitalLambda]1^2])/
+  (-M1^2 + M2^2) + (\[CapitalLambda]1^4*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[\[CapitalLambda]1^2])/(2*(M1^2 - M2^2)^2) + 
+ (\[CapitalLambda]1^2*func[tr[U[1, 3]**U[3, 1]]]*Log[\[CapitalLambda]1^2])/
+  (-M1^2 + M3^2) + (\[CapitalLambda]1^4*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[\[CapitalLambda]1^2])/(2*(M1^2 - M3^2)^2) + 
+ (\[CapitalLambda]1^2*func[tr[U[1, 4]**U[4, 1]]]*Log[\[CapitalLambda]1^2])/
+  (-M1^2 + M4^2) + (\[CapitalLambda]1^4*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[\[CapitalLambda]1^2])/(2*(M1^2 - M4^2)^2) - 
+ (func[tr[U[1, 1]^2]]*Log[\[CapitalLambda]1^2])/2 - 
+ (M1^4*func[tr[U[1, 2]**U[2, 1]]]*Log[-M1^2 + M2^2 + \[CapitalLambda]1^2])/
+  (2*(M1^2 - M2^2)^2) + (M1^2*M2^2*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[-M1^2 + M2^2 + \[CapitalLambda]1^2])/(M1^2 - M2^2)^2 - 
+ (M2^4*func[tr[U[1, 2]**U[2, 1]]]*Log[-M1^2 + M2^2 + \[CapitalLambda]1^2])/
+  (2*(M1^2 - M2^2)^2) + (M1^2*\[CapitalLambda]1^2*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[-M1^2 + M2^2 + \[CapitalLambda]1^2])/(M1^2 - M2^2)^2 - 
+ (M2^2*\[CapitalLambda]1^2*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[-M1^2 + M2^2 + \[CapitalLambda]1^2])/(M1^2 - M2^2)^2 - 
+ (\[CapitalLambda]1^4*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[-M1^2 + M2^2 + \[CapitalLambda]1^2])/(2*(M1^2 - M2^2)^2) - 
+ (M1^4*func[tr[U[1, 3]**U[3, 1]]]*Log[-M1^2 + M3^2 + \[CapitalLambda]1^2])/
+  (2*(M1^2 - M3^2)^2) + (M1^2*M3^2*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[-M1^2 + M3^2 + \[CapitalLambda]1^2])/(M1^2 - M3^2)^2 - 
+ (M3^4*func[tr[U[1, 3]**U[3, 1]]]*Log[-M1^2 + M3^2 + \[CapitalLambda]1^2])/
+  (2*(M1^2 - M3^2)^2) + (M1^2*\[CapitalLambda]1^2*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[-M1^2 + M3^2 + \[CapitalLambda]1^2])/(M1^2 - M3^2)^2 - 
+ (M3^2*\[CapitalLambda]1^2*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[-M1^2 + M3^2 + \[CapitalLambda]1^2])/(M1^2 - M3^2)^2 - 
+ (\[CapitalLambda]1^4*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[-M1^2 + M3^2 + \[CapitalLambda]1^2])/(2*(M1^2 - M3^2)^2) - 
+ (M1^4*func[tr[U[1, 4]**U[4, 1]]]*Log[-M1^2 + M4^2 + \[CapitalLambda]1^2])/
+  (2*(M1^2 - M4^2)^2) + (M1^2*M4^2*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[-M1^2 + M4^2 + \[CapitalLambda]1^2])/(M1^2 - M4^2)^2 - 
+ (M4^4*func[tr[U[1, 4]**U[4, 1]]]*Log[-M1^2 + M4^2 + \[CapitalLambda]1^2])/
+  (2*(M1^2 - M4^2)^2) + (M1^2*\[CapitalLambda]1^2*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[-M1^2 + M4^2 + \[CapitalLambda]1^2])/(M1^2 - M4^2)^2 - 
+ (M4^2*\[CapitalLambda]1^2*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[-M1^2 + M4^2 + \[CapitalLambda]1^2])/(M1^2 - M4^2)^2 - 
+ (\[CapitalLambda]1^4*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[-M1^2 + M4^2 + \[CapitalLambda]1^2])/(2*(M1^2 - M4^2)^2) - 
+ (func[tr[D1^4]]*Log[\[CapitalLambda]2^2])/2 + 
+ \[CapitalLambda]2^2*func[tr[D1[\[Mu]15]^2]]*Log[\[CapitalLambda]2^2] - 
+ func[tr[D1^2**U[2, 2]]]*Log[\[CapitalLambda]2^2] + 
+ (\[CapitalLambda]2^2*func[tr[U[1, 2]**U[2, 1]]]*Log[\[CapitalLambda]2^2])/
+  (M1^2 - M2^2) + (\[CapitalLambda]2^4*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[\[CapitalLambda]2^2])/(2*(M1^2 - M2^2)^2) + 
+ (\[CapitalLambda]2^2*func[tr[U[2, 3]**U[3, 2]]]*Log[\[CapitalLambda]2^2])/
+  (-M2^2 + M3^2) + (\[CapitalLambda]2^4*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[\[CapitalLambda]2^2])/(2*(M2^2 - M3^2)^2) + 
+ (\[CapitalLambda]2^2*func[tr[U[2, 4]**U[4, 2]]]*Log[\[CapitalLambda]2^2])/
+  (-M2^2 + M4^2) + (\[CapitalLambda]2^4*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[\[CapitalLambda]2^2])/(2*(M2^2 - M4^2)^2) - 
+ (func[tr[U[2, 2]^2]]*Log[\[CapitalLambda]2^2])/2 - 
+ (M1^4*func[tr[U[1, 2]**U[2, 1]]]*Log[M1^2 - M2^2 + \[CapitalLambda]2^2])/
+  (2*(M1^2 - M2^2)^2) + (M1^2*M2^2*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[M1^2 - M2^2 + \[CapitalLambda]2^2])/(M1^2 - M2^2)^2 - 
+ (M2^4*func[tr[U[1, 2]**U[2, 1]]]*Log[M1^2 - M2^2 + \[CapitalLambda]2^2])/
+  (2*(M1^2 - M2^2)^2) - (M1^2*\[CapitalLambda]2^2*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[M1^2 - M2^2 + \[CapitalLambda]2^2])/(M1^2 - M2^2)^2 + 
+ (M2^2*\[CapitalLambda]2^2*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[M1^2 - M2^2 + \[CapitalLambda]2^2])/(M1^2 - M2^2)^2 - 
+ (\[CapitalLambda]2^4*func[tr[U[1, 2]**U[2, 1]]]*
+   Log[M1^2 - M2^2 + \[CapitalLambda]2^2])/(2*(M1^2 - M2^2)^2) - 
+ (M2^4*func[tr[U[2, 3]**U[3, 2]]]*Log[-M2^2 + M3^2 + \[CapitalLambda]2^2])/
+  (2*(M2^2 - M3^2)^2) + (M2^2*M3^2*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[-M2^2 + M3^2 + \[CapitalLambda]2^2])/(M2^2 - M3^2)^2 - 
+ (M3^4*func[tr[U[2, 3]**U[3, 2]]]*Log[-M2^2 + M3^2 + \[CapitalLambda]2^2])/
+  (2*(M2^2 - M3^2)^2) + (M2^2*\[CapitalLambda]2^2*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[-M2^2 + M3^2 + \[CapitalLambda]2^2])/(M2^2 - M3^2)^2 - 
+ (M3^2*\[CapitalLambda]2^2*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[-M2^2 + M3^2 + \[CapitalLambda]2^2])/(M2^2 - M3^2)^2 - 
+ (\[CapitalLambda]2^4*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[-M2^2 + M3^2 + \[CapitalLambda]2^2])/(2*(M2^2 - M3^2)^2) - 
+ (M2^4*func[tr[U[2, 4]**U[4, 2]]]*Log[-M2^2 + M4^2 + \[CapitalLambda]2^2])/
+  (2*(M2^2 - M4^2)^2) + (M2^2*M4^2*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[-M2^2 + M4^2 + \[CapitalLambda]2^2])/(M2^2 - M4^2)^2 - 
+ (M4^4*func[tr[U[2, 4]**U[4, 2]]]*Log[-M2^2 + M4^2 + \[CapitalLambda]2^2])/
+  (2*(M2^2 - M4^2)^2) + (M2^2*\[CapitalLambda]2^2*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[-M2^2 + M4^2 + \[CapitalLambda]2^2])/(M2^2 - M4^2)^2 - 
+ (M4^2*\[CapitalLambda]2^2*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[-M2^2 + M4^2 + \[CapitalLambda]2^2])/(M2^2 - M4^2)^2 - 
+ (\[CapitalLambda]2^4*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[-M2^2 + M4^2 + \[CapitalLambda]2^2])/(2*(M2^2 - M4^2)^2) - 
+ (func[tr[D1^4]]*Log[\[CapitalLambda]3^2])/2 + 
+ \[CapitalLambda]3^2*func[tr[D1[\[Mu]16]^2]]*Log[\[CapitalLambda]3^2] - 
+ func[tr[D1^2**U[3, 3]]]*Log[\[CapitalLambda]3^2] + 
+ (\[CapitalLambda]3^2*func[tr[U[1, 3]**U[3, 1]]]*Log[\[CapitalLambda]3^2])/
+  (M1^2 - M3^2) + (\[CapitalLambda]3^4*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[\[CapitalLambda]3^2])/(2*(M1^2 - M3^2)^2) + 
+ (\[CapitalLambda]3^2*func[tr[U[2, 3]**U[3, 2]]]*Log[\[CapitalLambda]3^2])/
+  (M2^2 - M3^2) + (\[CapitalLambda]3^4*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[\[CapitalLambda]3^2])/(2*(M2^2 - M3^2)^2) + 
+ (\[CapitalLambda]3^2*func[tr[U[3, 4]**U[4, 3]]]*Log[\[CapitalLambda]3^2])/
+  (-M3^2 + M4^2) + (\[CapitalLambda]3^4*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[\[CapitalLambda]3^2])/(2*(M3^2 - M4^2)^2) - 
+ (func[tr[U[3, 3]^2]]*Log[\[CapitalLambda]3^2])/2 - 
+ (M1^4*func[tr[U[1, 3]**U[3, 1]]]*Log[M1^2 - M3^2 + \[CapitalLambda]3^2])/
+  (2*(M1^2 - M3^2)^2) + (M1^2*M3^2*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[M1^2 - M3^2 + \[CapitalLambda]3^2])/(M1^2 - M3^2)^2 - 
+ (M3^4*func[tr[U[1, 3]**U[3, 1]]]*Log[M1^2 - M3^2 + \[CapitalLambda]3^2])/
+  (2*(M1^2 - M3^2)^2) - (M1^2*\[CapitalLambda]3^2*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[M1^2 - M3^2 + \[CapitalLambda]3^2])/(M1^2 - M3^2)^2 + 
+ (M3^2*\[CapitalLambda]3^2*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[M1^2 - M3^2 + \[CapitalLambda]3^2])/(M1^2 - M3^2)^2 - 
+ (\[CapitalLambda]3^4*func[tr[U[1, 3]**U[3, 1]]]*
+   Log[M1^2 - M3^2 + \[CapitalLambda]3^2])/(2*(M1^2 - M3^2)^2) - 
+ (M2^4*func[tr[U[2, 3]**U[3, 2]]]*Log[M2^2 - M3^2 + \[CapitalLambda]3^2])/
+  (2*(M2^2 - M3^2)^2) + (M2^2*M3^2*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[M2^2 - M3^2 + \[CapitalLambda]3^2])/(M2^2 - M3^2)^2 - 
+ (M3^4*func[tr[U[2, 3]**U[3, 2]]]*Log[M2^2 - M3^2 + \[CapitalLambda]3^2])/
+  (2*(M2^2 - M3^2)^2) - (M2^2*\[CapitalLambda]3^2*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[M2^2 - M3^2 + \[CapitalLambda]3^2])/(M2^2 - M3^2)^2 + 
+ (M3^2*\[CapitalLambda]3^2*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[M2^2 - M3^2 + \[CapitalLambda]3^2])/(M2^2 - M3^2)^2 - 
+ (\[CapitalLambda]3^4*func[tr[U[2, 3]**U[3, 2]]]*
+   Log[M2^2 - M3^2 + \[CapitalLambda]3^2])/(2*(M2^2 - M3^2)^2) - 
+ (M3^4*func[tr[U[3, 4]**U[4, 3]]]*Log[-M3^2 + M4^2 + \[CapitalLambda]3^2])/
+  (2*(M3^2 - M4^2)^2) + (M3^2*M4^2*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[-M3^2 + M4^2 + \[CapitalLambda]3^2])/(M3^2 - M4^2)^2 - 
+ (M4^4*func[tr[U[3, 4]**U[4, 3]]]*Log[-M3^2 + M4^2 + \[CapitalLambda]3^2])/
+  (2*(M3^2 - M4^2)^2) + (M3^2*\[CapitalLambda]3^2*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[-M3^2 + M4^2 + \[CapitalLambda]3^2])/(M3^2 - M4^2)^2 - 
+ (M4^2*\[CapitalLambda]3^2*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[-M3^2 + M4^2 + \[CapitalLambda]3^2])/(M3^2 - M4^2)^2 - 
+ (\[CapitalLambda]3^4*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[-M3^2 + M4^2 + \[CapitalLambda]3^2])/(2*(M3^2 - M4^2)^2) - 
+ (func[tr[D1^4]]*Log[\[CapitalLambda]4^2])/2 + 
+ \[CapitalLambda]4^2*func[tr[D1[\[Mu]17]^2]]*Log[\[CapitalLambda]4^2] - 
+ func[tr[D1^2**U[4, 4]]]*Log[\[CapitalLambda]4^2] + 
+ (\[CapitalLambda]4^2*func[tr[U[1, 4]**U[4, 1]]]*Log[\[CapitalLambda]4^2])/
+  (M1^2 - M4^2) + (\[CapitalLambda]4^4*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[\[CapitalLambda]4^2])/(2*(M1^2 - M4^2)^2) + 
+ (\[CapitalLambda]4^2*func[tr[U[2, 4]**U[4, 2]]]*Log[\[CapitalLambda]4^2])/
+  (M2^2 - M4^2) + (\[CapitalLambda]4^4*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[\[CapitalLambda]4^2])/(2*(M2^2 - M4^2)^2) + 
+ (\[CapitalLambda]4^2*func[tr[U[3, 4]**U[4, 3]]]*Log[\[CapitalLambda]4^2])/
+  (M3^2 - M4^2) + (\[CapitalLambda]4^4*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[\[CapitalLambda]4^2])/(2*(M3^2 - M4^2)^2) - 
+ (func[tr[U[4, 4]^2]]*Log[\[CapitalLambda]4^2])/2 - 
+ (M1^4*func[tr[U[1, 4]**U[4, 1]]]*Log[M1^2 - M4^2 + \[CapitalLambda]4^2])/
+  (2*(M1^2 - M4^2)^2) + (M1^2*M4^2*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[M1^2 - M4^2 + \[CapitalLambda]4^2])/(M1^2 - M4^2)^2 - 
+ (M4^4*func[tr[U[1, 4]**U[4, 1]]]*Log[M1^2 - M4^2 + \[CapitalLambda]4^2])/
+  (2*(M1^2 - M4^2)^2) - (M1^2*\[CapitalLambda]4^2*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[M1^2 - M4^2 + \[CapitalLambda]4^2])/(M1^2 - M4^2)^2 + 
+ (M4^2*\[CapitalLambda]4^2*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[M1^2 - M4^2 + \[CapitalLambda]4^2])/(M1^2 - M4^2)^2 - 
+ (\[CapitalLambda]4^4*func[tr[U[1, 4]**U[4, 1]]]*
+   Log[M1^2 - M4^2 + \[CapitalLambda]4^2])/(2*(M1^2 - M4^2)^2) - 
+ (M2^4*func[tr[U[2, 4]**U[4, 2]]]*Log[M2^2 - M4^2 + \[CapitalLambda]4^2])/
+  (2*(M2^2 - M4^2)^2) + (M2^2*M4^2*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[M2^2 - M4^2 + \[CapitalLambda]4^2])/(M2^2 - M4^2)^2 - 
+ (M4^4*func[tr[U[2, 4]**U[4, 2]]]*Log[M2^2 - M4^2 + \[CapitalLambda]4^2])/
+  (2*(M2^2 - M4^2)^2) - (M2^2*\[CapitalLambda]4^2*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[M2^2 - M4^2 + \[CapitalLambda]4^2])/(M2^2 - M4^2)^2 + 
+ (M4^2*\[CapitalLambda]4^2*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[M2^2 - M4^2 + \[CapitalLambda]4^2])/(M2^2 - M4^2)^2 - 
+ (\[CapitalLambda]4^4*func[tr[U[2, 4]**U[4, 2]]]*
+   Log[M2^2 - M4^2 + \[CapitalLambda]4^2])/(2*(M2^2 - M4^2)^2) - 
+ (M3^4*func[tr[U[3, 4]**U[4, 3]]]*Log[M3^2 - M4^2 + \[CapitalLambda]4^2])/
+  (2*(M3^2 - M4^2)^2) + (M3^2*M4^2*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[M3^2 - M4^2 + \[CapitalLambda]4^2])/(M3^2 - M4^2)^2 - 
+ (M4^4*func[tr[U[3, 4]**U[4, 3]]]*Log[M3^2 - M4^2 + \[CapitalLambda]4^2])/
+  (2*(M3^2 - M4^2)^2) - (M3^2*\[CapitalLambda]4^2*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[M3^2 - M4^2 + \[CapitalLambda]4^2])/(M3^2 - M4^2)^2 + 
+ (M4^2*\[CapitalLambda]4^2*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[M3^2 - M4^2 + \[CapitalLambda]4^2])/(M3^2 - M4^2)^2 - 
+ (\[CapitalLambda]4^4*func[tr[U[3, 4]**U[4, 3]]]*
+   Log[M3^2 - M4^2 + \[CapitalLambda]4^2])/(2*(M3^2 - M4^2)^2)
