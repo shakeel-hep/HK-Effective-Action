648ba757e46c1d87fb5f242f3bb9223020d6aaed
Ramkumar <47427863+kaanapuliramkumar@users.noreply.github.com>
2023-11-27 13:16:04 +0530

    Add files via upload
    Open derivative structures of the effective action, separated class wise D^(r)U^(s) for F6M2.

---
+++ Effective operators (in open derivative form)/For 2 particles/SA6M2/D6U0_SA6M2.m
@@ -0,0 +1,16 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+func[tr[D1^2**D1[xz1]**D1^2**D1[xz1]]]/(30*\[CapitalLambda]1^2) + 
+ func[tr[D1^2**D1[xz1]**D1^2**D1[xz1]]]/(30*\[CapitalLambda]2^2) + 
+ (7*func[tr[D1^2**D1[xz1]**D1[xz2]**D1[xz1]**D1[xz2]]])/
+  (90*\[CapitalLambda]1^2) + 
+ (7*func[tr[D1^2**D1[xz1]**D1[xz2]**D1[xz1]**D1[xz2]]])/
+  (90*\[CapitalLambda]2^2) + 
+ func[tr[D1[xz1]**D1[xz2]**D1[xz1]**D1[xz3]**D1[xz2]**D1[xz3]]]/
+  (90*\[CapitalLambda]1^2) + 
+ func[tr[D1[xz1]**D1[xz2]**D1[xz1]**D1[xz3]**D1[xz2]**D1[xz3]]]/
+  (90*\[CapitalLambda]2^2) + 
+ func[tr[D1[xz1]**D1[xz2]**D1[xz3]**D1[xz1]**D1[xz3]**D1[xz2]]]/
+  (90*\[CapitalLambda]1^2) + 
+ func[tr[D1[xz1]**D1[xz2]**D1[xz3]**D1[xz1]**D1[xz3]**D1[xz2]]]/
+  (90*\[CapitalLambda]2^2) + tr[D1^6]/(30*\[CapitalLambda]1^2) + 
+ tr[D1^6]/(30*\[CapitalLambda]2^2)
---
+++ Effective operators (in open derivative form)/For 2 particles/SA6M2/D8U0_SA6M2.m
@@ -0,0 +1,20 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+func[tr[D1^4**D1[xz1]**D1^2**D1[xz1]]]/(10*\[CapitalLambda]1^4) + 
+ func[tr[D1^4**D1[xz1]**D1^2**D1[xz1]]]/(10*\[CapitalLambda]2^4) + 
+ func[tr[D1^4**D1[xz1]**D1[xz2]**D1[xz1]**D1[xz2]]]/
+  (30*\[CapitalLambda]1^4) + 
+ func[tr[D1^4**D1[xz1]**D1[xz2]**D1[xz1]**D1[xz2]]]/
+  (30*\[CapitalLambda]2^4) + 
+ func[tr[D1^2**D1[xz1]**D1^2**D1[xz2]**D1[xz1]**D1[xz2]]]/
+  (30*\[CapitalLambda]1^4) + 
+ func[tr[D1^2**D1[xz1]**D1^2**D1[xz2]**D1[xz1]**D1[xz2]]]/
+  (30*\[CapitalLambda]2^4) + 
+ func[tr[D1^2**D1[xz1]**D1[xz2]**D1^2**D1[xz1]**D1[xz2]]]/
+  (60*\[CapitalLambda]1^4) + 
+ func[tr[D1^2**D1[xz1]**D1[xz2]**D1^2**D1[xz1]**D1[xz2]]]/
+  (60*\[CapitalLambda]2^4) + 
+ func[tr[D1^2**D1[xz1]**D1[xz2]**D1^2**D1[xz2]**D1[xz1]]]/
+  (60*\[CapitalLambda]1^4) + 
+ func[tr[D1^2**D1[xz1]**D1[xz2]**D1^2**D1[xz2]**D1[xz1]]]/
+  (60*\[CapitalLambda]2^4) + tr[D1^8]/(20*\[CapitalLambda]1^4) + 
+ tr[D1^8]/(20*\[CapitalLambda]2^4)
